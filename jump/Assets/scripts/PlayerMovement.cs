c39828dd8db9fbc193b21e58269322df66c82704
GhoulMac <adimac.2002@gmail.com>
2022-02-02 01:03:47 +0530

    collective goods  and animation

--- jump/Assets/scripts/PlayerMovement.cs
+++ jump/Assets/scripts/PlayerMovement.cs
@@ -5,19 +5,26 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private BoxCollider2D coll;
     private SpriteRenderer sprite;
     private Animator anis;
 
     private float dirX = 0;
 
     [SerializeField] private float movespeed = 7f;
-    [SerializeField]private float jumpforce = 14f;
+    [SerializeField]private float jumpforce = 7f;
+
+    private enum MovementState { idle, running, falling, jumping }
+
+    [SerializeField] private LayerMask jumpableGround;
+
     // Start is called before the first frame update
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
         anis = GetComponent<Animator>();
+        coll = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -25,40 +32,43 @@ public class PlayerMovement : MonoBehaviour
     {
         dirX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(dirX * movespeed,rb.velocity.y);
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && IsGrounded() )
         {
             rb.velocity = new Vector2(rb.velocity.x,jumpforce);
         }
-        if (dirX > 0f)
-        {
-            anis.SetBool("running", true);
-        }
-        else if (dirX < 0f)
-        {
-            anis.SetBool("running", true);
-        }
-        else 
-        {
-            anis.SetBool("running", false);
-        }
+
         UpdateAnimationState();
     }
 
     private void UpdateAnimationState()
     {
+        MovementState state;
         if (dirX > 0f)
         {
-            anis.SetBool("running", true);
+            state = MovementState.running;
             sprite.flipX = false;
         }
         else if (dirX < 0f)
         {
-            anis.SetBool("running", true);
+            state = MovementState.running;
             sprite.flipX = true;
         }
         else
         {
-            anis.SetBool("running", false);
+            state = MovementState.idle;
         }
+        if (rb.velocity.y > .1f)
+        {
+            state = MovementState.jumping;
+        }
+        else if (rb.velocity.y < -.1f) 
+        {
+            state = MovementState.falling;
+        }
+        anis.SetInteger("state",(int)state);
+    }
+    private bool IsGrounded() 
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f,jumpableGround );
     }
 }
